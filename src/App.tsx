eec98dbf73bc986d2652f638c07d09e1d4d76e6d
Ramiro Capdevila <ramirocapdevila23@gmail.com>
2023-04-04 23:16:52 -0300

    agree NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+enum SelectedPage{
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus"
+}
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <div className="app bg-gray-20">
 
-      <Navbar />
+      <Navbar 
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
 
     </div>
   )
