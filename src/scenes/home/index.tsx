ceaa71f226eea5b455d9c6e8b76f79350c04b2a5
Ramiro Capdevila <ramirocapdevila23@gmail.com>
2023-04-13 23:29:43 -0300

    finalizado el home

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -21,7 +21,10 @@ const Home = ({ setSelectedPage }: Props) => {
     className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
     >
         {/* Image  and Main Header*/}
-        <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+        <motion.div 
+            className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+            onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        >
             {/* main header */}
             <div className="z-10 mt-32 md:basis-3/5">
              {/* Headings */}
@@ -49,7 +52,17 @@ const Home = ({ setSelectedPage }: Props) => {
                 </p>
               </motion.div>
               {/* Actions */}
-              <div className="mt-8 flex items-center gap-8">
+              <motion.div 
+                className="mt-8 flex items-center gap-8"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+                }}
+              >
                 <ActionButton setSelectedPage = {setSelectedPage}>
                     Join Now
                 </ActionButton>
@@ -60,7 +73,7 @@ const Home = ({ setSelectedPage }: Props) => {
                 >
                     <p>Learn More</p>
                 </AnchorLink>
-                </div>
+                </motion.div>
             </div>
 
             {/* Image */}
@@ -68,7 +81,7 @@ const Home = ({ setSelectedPage }: Props) => {
                 md:ml-40 md:mt-16 md:justify-items-end">
                 <img src={HomePageGhaphic} alt="home-pageGraphic" />
             </div>  
-        </div>
+        </motion.div>
 
         {/* Sponsors */}
         {isAboveMediumScreens && (
